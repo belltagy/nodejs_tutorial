e62bbf2aef0293826017b7533281c6552fa483d5
beltagy <moh.bell@crowdbotics.com>
2022-09-20 11:35:50 +0200

    add async awiat to read write file with help of util module

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
-const _ =require('lodash')
+const {readFile, writeFile} = require('fs');
+const util = require('util')
+const readFilepromise = util.promisify(readFile)
+const writeFilePromise = util.promisify(writeFile)
 
-const items= [1,[2,[3,[4]]]]
-const newitems = _.flattenDeep(items)
-console.log(newitems)
+// const getText = (path) =>{
+//     return new Promise((resolve, reject)=>{
+//         readFile(path, 'utf8',(err, data) =>{
+//             //setTimeout(()=>{return },1000)
+//             if (err){
+//                 reject(err)
+//             }
+//             else
+//             {
+//                 resolve(data)
+//                 console.log(data)
+//             }
+//         })
+//     })
+// }
+
+// getText('./content/first.txt')
+// .then((result) => console.log(result))
+// .catch((err) => console.log(err))
+
+const start = async() =>{
+    try{
+        const first = await readFilepromise('./content/first.txt', 'utf8');
+        const second = await readFilepromise('./content/second.txt', 'utf-8');
+        await writeFilePromise('./content/result-mind-grenade.txt',`this is file 1${first} ${second}`)
+        console.log(first, second)
+        }
+    catch (error){
+        console.log(error)
+    }
+}
+start()
